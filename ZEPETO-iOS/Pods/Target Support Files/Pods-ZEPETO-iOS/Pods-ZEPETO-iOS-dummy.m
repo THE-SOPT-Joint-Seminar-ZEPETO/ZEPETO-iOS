9a5233cfa783b9a843b89ebb3923866e78e1eca4
eunjuchoi <eunjuzzoo@naver.com>
2022-05-16 22:51:27 +0900

    [DEL] 프로젝트 파일 삭제

--- ZEPETO-iOS/Pods/Target Support Files/Pods-ZEPETO-iOS/Pods-ZEPETO-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZEPETO_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_ZEPETO_iOS
-@end
--- ZEPETO-iOS/ZEPETO-iOS/Global/PublicData/AppModel/AppContentDataModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AppContentDataModel.swift
-//  ZEPETO-iOS
-//
-//  Created by EUNJU on 2022/05/16.
-//
-
-import Foundation
