683b845af18f0e263c11c42cfa50a1fc265e1743
juhyeon <wngus4296@swu.ac.kr>
2022-05-17 13:31:15 +0900

    [FEAT] TV헤더뷰 -> CV로 변경 구현

---
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/CreateMain/Cell/CVC/Category/CategoryCVC.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCVC.swift
+//  ZEPETO-iOS
+//
+//  Created by 변주현 on 2022/05/17.
+//
+
+import UIKit
+
+class CategoryCVC: BaseCVC {
+
+    // MARK: IBOutlet
+    @IBOutlet weak var categoryBtn: UIButton!
+    
+    // MARK: Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- ZEPETO-iOS/ZEPETO-iOS/Screen/CreateMain/VC/CreateMainVC.swift
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/CreateMain/VC/CreateMainVC.swift
@@ -8,22 +8,103 @@
 import UIKit
 
 class CreateMainVC: BaseVC {
-
+    
+    // MARK: IBOutlet
+    @IBOutlet weak var postTV: UITableView!
+    @IBOutlet weak var categoryCV: UICollectionView!
+    
+    // MARK: Properties
+    var categoryList = ["MY", "HOT", "NEW", "템플릿", "커플", "Photo"]
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setPostTV()
+        setCategoryCV()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Custom Methods
+extension CreateMainVC {
+    
+    /// TV 관련 설정하는 메서드
+    private func setPostTV() {
+        let nib = UINib(nibName: PostTVC.className, bundle: nil)
+        postTV.register(nib, forCellReuseIdentifier: PostTVC.className)
+        postTV.delegate = self
+        postTV.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setCategoryCV() {
+        let nib = UINib(nibName: CategoryCVC.className, bundle: nil)
+        categoryCV.register(nib, forCellWithReuseIdentifier: CategoryCVC.className)
+        
+        categoryCV.delegate = self
+        categoryCV.dataSource = self
     }
-    */
+}
 
+// MARK: - UITableViewDelegate
+extension CreateMainVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension CreateMainVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return PostDataModel.sampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTVC.className, for: indexPath) as? PostTVC else { return UITableViewCell() }
+        
+        cell.setData(PostDataModel.sampleData[indexPath.row])
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension CreateMainVC: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoryList.count
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension CreateMainVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCVC.className, for: indexPath) as? CategoryCVC else {
+            return UICollectionViewCell()
+        }
+        cell.categoryBtn.setTitle(categoryList[indexPath.item], for: .normal)
+        if (indexPath.item == 1) {
+            cell.categoryBtn.setTitleColor(.white, for: .normal)
+            cell.categoryBtn.setBackgroundColor(.gray900, for: .normal)
+        }
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension CreateMainVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        
+        let cellWidth = width * (47/375)
+        let cellHeight = cellWidth * (32/176)
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 14, left: 18, bottom: 14, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
 }
