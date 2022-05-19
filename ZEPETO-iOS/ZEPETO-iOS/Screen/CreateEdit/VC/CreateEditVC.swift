4a8b0b172ab7f6afcf2e67ba4f0855d258ba6124
juhyeon <wngus4296@swu.ac.kr>
2022-05-19 18:21:59 +0900

    [ADD] CreateEditSB, VC 생성

---
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/CreateEdit/VC/CreateEditVC.swift
@@ -0,0 +1,16 @@
+//
+//  CreateEditVC.swift
+//  ZEPETO-iOS
+//
+//  Created by 변주현 on 2022/05/19.
+//
+
+import UIKit
+
+class CreateEditVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
