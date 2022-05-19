9aede9281d49ad38aedcee87fbab309f06f0cdf9
jeongkite <jeong6kite@gmail.com>
2022-05-20 02:21:48 +0900

    [FEAT] TextView Placeholder 적용 (#2)

--- ZEPETO-iOS/ZEPETO-iOS/Screen/CreateUpload/VC/CreateUploadVC.swift
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/CreateUpload/VC/CreateUploadVC.swift
@@ -19,27 +19,26 @@ class CreateUploadVC: BaseVC {
         super.viewDidLoad()
         configUI()
     }
-}
-
-// MARK: - UI
-extension CreateUploadVC {
+    
+    // [?] 궁금한게 이거 스보에도 똑같이 컬러 gray300으로 설정했는데 뷰컨에서 아래처럼 따로 설정해주지 않으면 밑에 placeholder 설정이 안 먹더라구요? 왜그럴까요?? 원래 그런건가요???🤔
+    // MARK: UI
     func configUI() {
-        mainImgView.makeRounded(cornerRadius: 10)
-        hashTagView.makeRounded(cornerRadius: nil)
+        mainTextView.textColor = .gray300
+        mainTextView.delegate = self
     }
 }
 
 // MARK: - textView placeholder 설정
-extension CreateUploadVC {
+extension CreateUploadVC: UITextViewDelegate {
     /// 텍스트뷰 편집 시작시 placeholder 텍스트를 없애고 색상을 어둡게 변경하는 메서드
-    @objc func textViewDidBeginEditing(_ textView: UITextView) {
+    func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.textColor == .gray300 {
             textView.text = nil
             textView.textColor = .gray800
         }
     }
     /// 텍스트뷰 편집 종료시 비어있으면 placeholder 텍스트를 추가하는 메서드
-    @objc func textViewDidEndEditing(_ textView: UITextView) {
+    func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.isEmpty {
             textView.text = "이야기를 완성하세요"
             textView.textColor = .gray300
