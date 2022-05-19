54b7300138f6523eae279cdf98b39e4fa1de3d79
juhyeon <wngus4296@swu.ac.kr>
2022-05-19 12:29:30 +0900

    [CHORE] 샘플 데이터 변경

--- ZEPETO-iOS/ZEPETO-iOS/Screen/CreateMain/Cell/TVC/PostDataModel.swift
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/CreateMain/Cell/TVC/PostDataModel.swift
@@ -20,9 +20,9 @@ struct PostDataModel {
 extension PostDataModel {
     static let sampleData: [PostDataModel] = [
         PostDataModel(profileImgName: "writerThumbnail", title: "ZEPETO 신상 포즈", description: "제일 먼저 해보세요!", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
-        PostDataModel(profileImgName: "writerThumbnail", title: "트렌드 포즈", description: "최신 트렌드 한 눈에 보기!", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
-        PostDataModel(profileImgName: "writerThumbnail", title: "ZEPETO 신상 포즈", description: "제일 먼저 해보세요!", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
-        PostDataModel(profileImgName: "writerThumbnail", title: "트렌드 포즈", description: "최신 트렌드 한 눈에 보기!", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
-        PostDataModel(profileImgName: "writerThumbnail", title: "ZEPETO 신상 포즈", description: "제일 먼저 해보세요!", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
+        PostDataModel(profileImgName: "writerThumbnail", title: "트렌드 포즈", description: "최신 트렌드 한 눈에 보기!", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
+        PostDataModel(profileImgName: "writerThumbnail", title: "Minnie Mouse", description: "Special Event Booth🎀", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
+        PostDataModel(profileImgName: "writerThumbnail", title: "Nice to CU♥", description: "CU in a ZEPETO", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
+        PostDataModel(profileImgName: "writerThumbnail", title: "SF9", description: "To be Sensation SF9!", imgList: [ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail"), ImgDataModel(imgName: "posethumbnail")]),
     ]
 }
