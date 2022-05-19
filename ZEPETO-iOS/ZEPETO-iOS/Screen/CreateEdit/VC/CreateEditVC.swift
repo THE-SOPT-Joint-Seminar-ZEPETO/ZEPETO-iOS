d2914bf5b5015af851570e31324c44ab43258d05
juhyeon <wngus4296@swu.ac.kr>
2022-05-19 22:12:18 +0900

    [FEAT] CreateEdit UI 구현

--- ZEPETO-iOS/ZEPETO-iOS/Screen/CreateEdit/VC/CreateEditVC.swift
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/CreateEdit/VC/CreateEditVC.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class CreateEditVC: BaseVC {
 
+    // MARK: IBOutlet
+    @IBOutlet weak var img: UIImageView!
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // MARK: IBAction
+    @IBAction func TapCloseBtn(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func TapNextBtn(_ sender: Any) {
+        // TODO: 다음 VC로 이미지 전달, push로 구현
+    }
+}
+
+// MARK: - Custom Methods
+extension CreateEditVC {
+    private func setImg() {
+    }
 }
