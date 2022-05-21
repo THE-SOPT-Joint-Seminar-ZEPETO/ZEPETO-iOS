e8c0ddb5c0bcb4d8e8f6af6eb3c914a5e337faea
jeongkite <jeong6kite@gmail.com>
2022-05-21 23:30:03 +0900

    [FEAT] 피드 텍스트에 그림자 추가 (#4)

--- ZEPETO-iOS/ZEPETO-iOS/Screen/Feed/VC/FeedVC.swift
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/Feed/VC/FeedVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class FeedVC: BaseVC {
     
     // MARK: IBOutlet
+    @IBOutlet weak var usernameTextLabel: UILabel!
     @IBOutlet weak var mainTextLabel: UILabel!
     @IBOutlet weak var profileImgView: UIImageView!
     
@@ -26,12 +27,29 @@ extension FeedVC {
         profileImgView.layer.borderWidth = 2
         profileImgView.layer.borderColor = UIColor.white.cgColor
         profileImgView.layer.cornerRadius = profileImgView.bounds.width / 2
+        
+        usernameTextLabel.textDropShadow()
+        mainTextLabel.textDropShadow()
     }
 }
 
 // MARK: - Custom Methods
 extension FeedVC {
+    
+    /// status bar를 흰색으로 바꿔주는 메서드
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+}
+
+// MARK: - [?] 여기다 뭐라고 더 달아야 할까요??
+extension UILabel {
+    
+    /// 텍스트에 그림자를 주는 메서드
+    func textDropShadow() {
+        self.layer.masksToBounds = false
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowOffset = CGSize(width: 1, height: 1)
     }
 }
