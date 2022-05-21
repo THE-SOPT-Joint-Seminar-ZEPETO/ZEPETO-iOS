084b87f79bd3290371d79c58141c2746bb2976d8
jeongkite <jeong6kite@gmail.com>
2022-05-22 01:24:21 +0900

    [FEAT] 피드 메인 텍스트 더보기/숨기기 구현 (#4)

--- ZEPETO-iOS/ZEPETO-iOS/Screen/Feed/VC/FeedVC.swift
+++ ZEPETO-iOS/ZEPETO-iOS/Screen/Feed/VC/FeedVC.swift
@@ -13,11 +13,31 @@ class FeedVC: BaseVC {
     @IBOutlet weak var usernameTextLabel: UILabel!
     @IBOutlet weak var mainTextLabel: UILabel!
     @IBOutlet weak var profileImgView: UIImageView!
+    @IBOutlet weak var moreBtn: UIButton!
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configUI()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configMoreBtnHide()
+    }
+    
+    // MARK: IBAction
+    @IBAction func tapMoreBtn(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            mainTextLabel.numberOfLines = 0
+            mainTextLabel.invalidateIntrinsicContentSize()
+            moreBtn.setTitle("숨기기", for: .normal)
+        } else {
+            configMainTextLabelShort()
+            mainTextLabel.invalidateIntrinsicContentSize()
+            moreBtn.setTitle("더보기", for: .normal)
+        }
     }
 }
 
@@ -30,6 +50,7 @@ extension FeedVC {
         
         usernameTextLabel.textDropShadow()
         mainTextLabel.textDropShadow()
+        configMainTextLabelShort()
     }
 }
 
@@ -39,6 +60,20 @@ extension FeedVC {
     /// status bar를 흰색으로 바꿔주는 메서드
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    /// mainTextLabel을 2줄로 만드는 메서드
+    private func configMainTextLabelShort() {
+        mainTextLabel.numberOfLines = 2
+        mainTextLabel.lineBreakMode = .byTruncatingTail
+    }
+
+    /// mainTextLabel이 2줄 이하일 때 더보기 버튼을 숨기는 메서드
+    private func configMoreBtnHide() {
+        let lineCount = mainTextLabel.maxNumberOfLines
+        if lineCount <= 2 {
+            moreBtn.isHidden = true
+        }
     }
 }
 
@@ -51,5 +86,14 @@ extension UILabel {
         self.layer.shadowRadius = 2.0
         self.layer.shadowOpacity = 0.2
         self.layer.shadowOffset = CGSize(width: 1, height: 1)
+    }
+    
+    /// UILabel의 총 Line 수를 구하는 메서드
+    var maxNumberOfLines: Int {
+        let maxSize = CGSize(width: frame.size.width, height: CGFloat(MAXFLOAT))
+        let text = (self.text ?? "") as NSString
+        let textHeight = text.boundingRect(with: maxSize, options: .usesLineFragmentOrigin, attributes: [.font: font!], context: nil).height
+        let lineHeight = font.lineHeight
+        return Int(ceil(textHeight / lineHeight))
     }
 }
