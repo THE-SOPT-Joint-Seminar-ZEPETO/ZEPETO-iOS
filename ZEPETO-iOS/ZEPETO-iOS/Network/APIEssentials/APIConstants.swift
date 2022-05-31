d96442faa954f61f1ee8b9e1e7aa9d1252572321
eunjuchoi <eunjuzzoo@naver.com>
2022-05-31 12:11:23 +0900

    [ADD] 네트워크 통신을 위한 기초 세팅 (#14)

---
+++ ZEPETO-iOS/ZEPETO-iOS/Network/APIEssentials/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  ZEPETO-iOS
+//
+//  Created by EUNJU on 2022/05/31.
+//
+
+import Foundation
+
+struct APIConstants {
+    
+    // MARK: - Base URL
+    static let baseURL = "http://3.39.190.107:5000"
+}
