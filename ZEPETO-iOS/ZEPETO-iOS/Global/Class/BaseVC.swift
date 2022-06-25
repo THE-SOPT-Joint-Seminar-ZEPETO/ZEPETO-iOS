504cbfd29420d4dd6d7fec6444d1c34c3acd4a08
eunjuchoi <eunjuzzoo@naver.com>
2022-06-25 13:45:04 +0900

    [ADD] Lottie 설정 (#11)

--- ZEPETO-iOS/ZEPETO-iOS/Global/Class/BaseVC.swift
+++ ZEPETO-iOS/ZEPETO-iOS/Global/Class/BaseVC.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import Lottie
 
 class BaseVC: UIViewController {
 
+    // MARK: Properties
+    let loadingView = AnimationView(name: "loading_lottie")
+    let checkView = AnimationView(name: "check_lottie")
+    
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +29,43 @@ extension BaseVC {
     
     func showTabbar() {
         self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    func setLottie() {
+        loadingView.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
+        loadingView.center = self.view.center
+        loadingView.contentMode = .scaleAspectFit
+        loadingView.loopMode = .loop
+        loadingView.isHidden = true
+        view.addSubview(loadingView)
+        
+        checkView.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
+        checkView.center = self.view.center
+        checkView.contentMode = .scaleAspectFit
+        checkView.loopMode = .playOnce
+        checkView.isHidden = true
+        view.addSubview(checkView)
+    }
+    
+    func loadingPlay() {
+        loadingView.isHidden = false
+        loadingView.play()
+    }
+    
+    func loadingStop() {
+        loadingView.stop()
+        loadingView.isHidden = true
+    }
+    
+    func checkPlay(_ vc: UIViewController) {
+        checkView.isHidden = false
+        checkView.play() { _ in
+            vc.dismiss(animated: true)
+        }
+    }
+    
+    func checkStop() {
+        checkView.stop()
+        checkView.isHidden = true
     }
 }
